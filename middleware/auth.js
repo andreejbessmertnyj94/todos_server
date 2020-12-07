a910afb6b7257722b804d0a76bb5add13ae690c7
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-12-07 11:45:09 +0300

    refactor

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,23 +4,29 @@ const User = require("../modules/users/model");
 const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
+
   try {
-    if (token == null)
+    if (!token) {
       throw {
         name: "UnauthorizedError",
         message:
           "Attempt to access without the token, or an incorrect token format.",
       };
+    }
+
     const data = jwt.verify(token, process.env.JWT_KEY);
     const user = await User.findOne({ _id: data._id, "tokens.value": token });
+
     if (!user) {
       throw {
         name: "UnauthorizedError",
         message: "User from token not found",
       };
     }
+
     req.user = user;
     req.token = token;
+
     next();
   } catch (err) {
     return next(err);
